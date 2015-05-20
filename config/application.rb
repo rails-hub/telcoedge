68c7377000ea5f7649624d237bd755498033691f
todd <todd@todd.(none)>
2015-05-20 13:52:09 +0500

    disable assets precompile from app.rb

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,7 @@ module Telcoedge
     # config.active_record.whitelist_attributes = false
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
     # config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
